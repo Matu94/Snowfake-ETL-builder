c09d79f5531f93a3ec4f3d0a37189ba9451a8a61
Matu <agostonmate9@gmail.com>
2026-01-18 11:11:10 +0100

    git manager

---
+++ utils/git_manager.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from github import Github
+import base64
+
+def push_to_github(file_path, file_content, commit_message):
+    try:
+        #Connect
+        token = st.secrets["github"]["token"]
+        repo_name = st.secrets["github"]["repo_name"]
+        branch = st.secrets["github"]["branch"]
+        
+        g = Github(token)
+        repo = g.get_repo(repo_name)
+
+        #Check if file exists to decide: Create or Update?
+        try:
+            contents = repo.get_contents(file_path, ref=branch)
+            # UPDATE existing file
+            repo.update_file(
+                path=file_path,
+                message=commit_message,
+                content=file_content,
+                sha=contents.sha,
+                branch=branch
+            )
+            return f"Updated {file_path} on GitHub!"
+            
+        except:
+            #CREATE new file
+            repo.create_file(
+                path=file_path,
+                message=commit_message,
+                content=file_content,
+                branch=branch
+            )
+            return f"Created {file_path} on GitHub!"
+
+    except Exception as e:
+        return f"Git Error: {str(e)}"
